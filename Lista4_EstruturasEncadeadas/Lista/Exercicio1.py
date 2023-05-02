4abb2802c0c817beb62e4e7b83375a22cfba006d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 14:44:24 -0300

    Create Exercicio1.py

---
+++ Lista4_EstruturasEncadeadas/Lista/Exercicio1.py
@@ -0,0 +1,20 @@
+#1) Implemente a função remove utilizando a função busca. 
+
+
+
+def remove(lista, valor):
+    if lista is None:  # caso a lista esteja vazia
+        return lista
+    
+    if lista.dado == valor:  # caso o elemento a ser removido seja o primeiro
+        lista = lista.prox
+        return lista
+    
+    anterior = busca(lista, valor)  # busca o elemento anterior ao que será removido
+    if anterior is None:  # caso o valor não esteja na lista
+        return lista
+    
+    elemento = anterior.prox
+    anterior.prox = elemento.prox  # atualiza o ponteiro do elemento anterior
+    del elemento  # exclui o elemento
+    return lista
