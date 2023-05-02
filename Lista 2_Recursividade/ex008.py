083fdbb771806de186342110c9ff5af8d9857101
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 12:56:51 -0300

    Update ex008.py

--- Lista 2_Recursividade/ex008.py
+++ Lista 2_Recursividade/ex008.py
@@ -1,6 +1,6 @@
-# 8) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. Escreva uma função
-# recursiva mdc em python, que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é
-# igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.
+'''8) O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. Escreva uma função
+recursiva mdc em python, que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é
+igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto.'''
 
 def mdc(x: int, y: int) -> int:
     if y == 0:
@@ -12,4 +12,4 @@ def mdc(x: int, y: int) -> int:
 if __name__ == "__main__":
     x = int(input("Insira um número: "))
     y = int(input("Insira um número: "))
-    print(f"O máximo divisor comum de {x} e {y} é {mdc(x, y)}")
+    print(f"O MDC de {x} e {y} é {mdc(x, y)}")
