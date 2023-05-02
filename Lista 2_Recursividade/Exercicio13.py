3b865b2724b810da1cb6ee16f1d7c93898cba79d
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 13:12:19 -0300

    Update and rename ex013.py to Exercicio13.py

---
+++ Lista 2_Recursividade/Exercicio13.py
@@ -0,0 +1,14 @@
+# 13) Faça uma função recursiva que receba um número inteiro positivo N e imprima todos os números naturais de 0 até
+# N em ordem decrescente.
+
+def recursive_countup_recursive(start:int, finish: int) -> int:
+    print(start, end=" ")
+    if start <= finish:
+        return start
+    return recursive_countup_recursive((start - 1), finish)
+
+
+if __name__ == "__main__":
+    n = int(input("Digite um número: "))
+    recursive_countup_recursive(n, 0)
+    print(recursive_countup_recursive)
