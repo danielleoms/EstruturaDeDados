1a26edf9a11dd6d29661b1d5913eae81a4a83718
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 11:45:57 -0300

    Update and rename Exercicio3.py to Exercicio03.py

--- Lista1_POO/Exercicio03.py
+++ Lista1_POO/Exercicio03.py
@@ -1,4 +1,7 @@
-# Classe Retangulo
+# Classe Retangulo: Crie uma classe que modele um retangulo:
+
+# Atributos: LadoA, LadoB (ou Comprimento e Largura, ou Base e Altura, a escolher)
+# Métodos: Mudar valor dos lados, Retornar valor dos lados, calcular Área e calcular Perímetro;
 
 
 class Retangulo:
