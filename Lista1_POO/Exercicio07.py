4c8b86a5d4255d2bc21d300bc71cc5aec244e730
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 12:02:16 -0300

    Update and rename Exercicio7.py to Exercicio07.py

--- Lista1_POO/Exercicio07.py
+++ Lista1_POO/Exercicio07.py
@@ -1,4 +1,11 @@
-# Classe bichinho virtual
+# Classe Bichinho Virtual: Crie uma classe que modele um Tamagushi (Bichinho Eletrônico):
+
+# Atributos: Nome, Fome, Saúde e Idade
+# Métodos: Alterar Nome, Fome, Saúde e Idade; Retornar Nome, Fome, Saúde e Idade
+# Obs: Existe mais uma informação que devemos levar em consideração,
+# o Humor do nosso tamagushi, este humor é uma combinação entre os atributos Fome e Saúde,
+# ou seja, um campo calculado, então não devemos criar um atributo para armazenar esta informação
+# por que ela pode ser calculada a qualquer momento.
 
 class Bichinho:
     def __init__(self, nome, fome, saude, idade):
@@ -33,5 +40,3 @@ class Bichinho:
 
     def retornar_humor(self):
         return self.saude/2 + self.fome/2
-
-
