fa77478ca1c974e7f083c9eef1f4030228e3d378
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 15:48:44 -0300

    Update Exercicio3.py

--- Lista4_EstruturasEncadeadas/Pilha/Exercicio3.py
+++ Lista4_EstruturasEncadeadas/Pilha/Exercicio3.py
@@ -1,1 +1,12 @@
+# 3) Escreva uma função que receba como parâmetro duas pilhas e verifique de elas são iguais. Duas pilhas são iguais se possuem os mesmos elementos, na mesma ordem.
 
+def pilhas_sao_iguais(pilha1, pilha2):
+    # Se as pilhas têm tamanhos diferentes, elas não são iguais
+    if len(pilha1) != len(pilha2):
+        return False
+    # Verifica se cada elemento das pilhas é igual, na mesma ordem
+    for i in range(len(pilha1)):
+        if pilha1[i] != pilha2[i]:
+            return False
+    # Se chegou aqui, as pilhas são iguais
+    return True
