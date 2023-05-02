a343d7f17fc1d69d7cd4e9203f3fb1258d0e823b
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 13:38:56 -0300

    Update Exercicio01.py

--- Lista 3_Listas_Built-in/Exercicio01.py
+++ Lista 3_Listas_Built-in/Exercicio01.py
@@ -1,9 +1,15 @@
 # 1) Faça um Programa que leia um vetor de 5 números inteiros e mostre-os
+
 import random
 
+# cria uma lista vazia
 lista = []
+
+# itera 5 vezes, adicionando um número inteiro aleatório (entre 0 e 9) à lista a cada iteração
 for i in range(5):
     lista.append(random.randrange(10))
+
+# imprime a lista na tela, separando cada número por um espaço em branco
 print("A lista é: ", end="")
 for e in lista:
     print(e, end=" ")
