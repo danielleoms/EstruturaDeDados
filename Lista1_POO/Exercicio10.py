caaec2e41460c763be482aaed64e10228d506ab1
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 12:23:49 -0300

    Update Exercicio10.py

--- Lista1_POO/Exercicio10.py
+++ Lista1_POO/Exercicio10.py
@@ -1,4 +1,24 @@
-# Classe bomba de combustivel
+'''Classe Bomba de Combustível: Faça um programa completo utilizando classes e métodos que:
+    Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
+        tipoCombustivel.
+        valorLitro
+        quantidadeCombustivel
+
+    Possua no mínimo esses métodos:
+        abastecerPorValor( ) – método onde é informado o valor a ser abastecido e mostra a
+        quantidade de litros que foi colocada no veículo
+
+        abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível
+        e mostra o valor a ser pago pelo cliente.
+
+        alterarValor( ) – altera o valor do litro do combustível.
+
+        alterarCombustivel( ) – altera o tipo do combustível.
+
+        alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
+
+        OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de
+        combustível total na bomba. '''
 
 class BombaCombustivel:
     def __init__(self, tipoCombustivel, valorLitro, quantidadeCombustivel):
@@ -23,4 +43,3 @@ class BombaCombustivel:
     def abastecer_por_litros(self, litros):
         self.quantidadeCombustivel += litros
         return litros/self.valorLitro
-
