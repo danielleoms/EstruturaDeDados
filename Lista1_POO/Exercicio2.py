91f3b31d4b7de6756a557806c54310cdb709e0dd
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 11:42:53 -0300

    Update Exercicio2.py

--- Lista1_POO/Exercicio2.py
+++ Lista1_POO/Exercicio2.py
@@ -1,4 +1,7 @@
-# Classe Quadrado
+# Classe Quadrado: Crie uma classe que modele um quadrado:
+
+# Atributos: Tamanho do lado
+# Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
 class Quadrado:
     def __init__(self, lado):
@@ -12,4 +15,3 @@ class Quadrado:
 
     def area(self):
         return self.lado * self.lado
-
