da4203e0a1817436d8f37b84a5bde65553b3ab65
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 15:45:46 -0300

    Update Exercicio1.py

--- Lista4_EstruturasEncadeadas/Pilha/Exercicio1.py
+++ Lista4_EstruturasEncadeadas/Pilha/Exercicio1.py
@@ -1,1 +1,13 @@
+# 1) Escrever uma função que receba como parâmetro uma pilha. A função deve retornar o maior elemento da pilha.
 
+def maior_elemento(pilha):
+    maior = None
+    # Percorre toda a pilha
+    while not pilha.esta_vazia():
+        # Obtém o valor do topo da pilha
+        topo = pilha.desempilha()
+        # Verifica se é o maior valor encontrado até agora
+        if maior is None or topo > maior:
+            maior = topo
+    # Retorna o maior valor encontrado
+    return maior
