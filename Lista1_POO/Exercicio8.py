85f4a5faef202c181a65a86e8a160f2a42207963
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 12:19:33 -0300

    Update Exercicio8.py

--- Lista1_POO/Exercicio8.py
+++ Lista1_POO/Exercicio8.py
@@ -1,4 +1,9 @@
-# Classe Macaco
+# Classe Macaco: Desenvolva uma classe Macaco,que possua os atributos nome e bucho
+# (estomago) e pelo menos os métodos comer(), verBucho() e digerir().
+# Faça um programa ou teste interativamente, criando pelo menos dois macacos,
+# alimentando-os com pelo menos 3 alimentos diferentes e verificando o conteúdo do
+# estomago a cada refeição. Experimente fazer com que um macaco coma o outro.
+# É possível criar um macaco canibal?
 
 class Macaco:
     def __init__(self, nome, estomago=[]):
@@ -15,19 +20,19 @@ class Macaco:
         self.estomago.remove(comida)
 
 
-macaco1 = Macaco('Felix')
+macaco1 = Macaco('Chico')
 macaco1.comer("banana")
 print(macaco1.ver_bucho())
-macaco1.comer("maçã")
+macaco1.comer("ração")
 print(macaco1.ver_bucho())
-macaco1.digerir("maçã")
+macaco1.digerir("ração")
 print(macaco1.ver_bucho())
-macaco1.comer("chocolate")
+macaco1.comer("melancia")
 print(macaco1.ver_bucho())
-macaco1.digerir("chocolate")
+macaco1.digerir("melancia")
 print(macaco1.ver_bucho())
 
-macaco2 = Macaco('Xiita', ['laranja'])
+macaco2 = Macaco('Sabrina', ['kiwi'])
 print(macaco2.ver_bucho())
 macaco2.comer(macaco1)
 print(macaco2.ver_bucho())
