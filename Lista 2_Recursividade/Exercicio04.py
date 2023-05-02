1c2f5c40de041d20ea1389d6ef62670de225cce6
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-02 12:50:51 -0300

    Update and rename ex004.py to Exercicio04.py

--- Lista 2_Recursividade/Exercicio04.py
+++ Lista 2_Recursividade/Exercicio04.py
@@ -1,19 +1,5 @@
 # 4) Faça uma função recursiva que permita somar os elementos de um vetor de inteiros.
 
-# 1ª forma:
-def soma_vetor(vetor: list, n: int) -> int:
-    if n == 0:
-        return 0
-    return vetor[n - 1] + soma_vetor(vetor, n - 1)
-
-
-if __name__ == "__main__":
-    vetor = [1, 2, 3, 4]
-    print(f"A soma dos elementos do vetor {vetor} é {soma_vetor(vetor, len(vetor))}")
-
-
-# 2ª forma:
-
 def somaLista(lista: list) -> int:
     if not lista:
         return 0
