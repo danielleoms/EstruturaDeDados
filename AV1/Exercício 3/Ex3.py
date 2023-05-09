649db4dd0dc7d76eb896365143f11097084d44d5
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-09 17:00:36 -0300

    Update Ex3.py

--- AV1/Exercício 3/Ex3.py
+++ AV1/Exercício 3/Ex3.py
@@ -1,15 +1,52 @@
-# Escreva uma função em python para somar todos os valores de uma fila recursiva
+# Escreva uma função em Python para somar todos os valores de uma fila de forma recursiva
+
+class Nodo:
+    """ Esta classe representa um nodo em uma encadeada"""
+
+    def __init__(self, dado=0, proximo_nodo=None):
+        self.dado = dado
+        self.proximo = proximo_nodo
+
+    def __repr__(self):
+        return "%s -> %s" % (self.dado, self.proximo)
 
 
-def soma_fila_recursiva(fila):
-    if len(fila) == 0:  # caso base: fila vazia
+class Fila:
+    """ Esssa classe representa uma fila usando uma estrutura encadeada"""
+
+    def __init__(self):
+        self.primeiro = None
+        self.ultimo = None
+
+    def __repr__(self):
+        return "[" + str(self.primeiro) + "]"
+
+    def push(self, novo_dado):
+        """Inseri um elemento no final da fila"""
+
+        novo_nodo = Nodo(novo_dado)
+
+        if self.primeiro is None:
+            self.primeiro = novo_nodo
+            self.ultimo = novo_nodo
+        else:
+            self.ultimo.proximo = novo_nodo
+            self.ultimo = novo_nodo
+
+    def pop(self):
+        """Remove o primeiro elemento da fila"""
+
+        assert self.primeiro is not None, "Impossível remover elemento de fila vazia"
+
+        self.primeiro = self.primeiro.proximo
+        if self.primeiro is None:
+            self.ultimo = None
+
+
+def somaLista(fila: Fila) -> int:
+    if fila.primeiro is None:
         return 0
     else:
-        primeiro = fila[0]
-        resto = fila[1:]
-        return primeiro + soma_fila_recursiva(resto)
-
-fila = [1, 2, 3, 4, 5]
-resultado = soma_fila_recursiva(fila)
-print(resultado)  # output: 15
-
+        soma = fila.primeiro.dado
+        fila.pop()
+        return soma + somaLista(fila)
