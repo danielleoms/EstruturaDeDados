20a128f7f097fac757634b2a00dcf86d197ccaea
Danielle Moreno <101332797+danielleoms@users.noreply.github.com>
2023-05-09 18:15:22 -0300

    Update Ex3.py

--- AV1/Exercício 3/Ex3.py
+++ AV1/Exercício 3/Ex3.py
@@ -1,52 +1,62 @@
-# Escreva uma função em Python para somar todos os valores de uma fila de forma recursiva
+# Escreva uma função em python para somar todos os valores de uma fila recursiva
 
 class Nodo:
-    """ Esta classe representa um nodo em uma encadeada"""
+    """Esta classe representa um nó em uma lista encadeada"""
 
-    def __init__(self, dado=0, proximo_nodo=None):
+    def __init__(self, dado=0, proximo_no=None):
         self.dado = dado
-        self.proximo = proximo_nodo
+        self.proximo = proximo_no
 
     def __repr__(self):
-        return "%s -> %s" % (self.dado, self.proximo)
+        return f"{self.dado} -> {self.proximo}"
 
 
 class Fila:
-    """ Esssa classe representa uma fila usando uma estrutura encadeada"""
+    """Essa classe representa uma fila usando uma estrutura de lista encadeada"""
 
     def __init__(self):
         self.primeiro = None
         self.ultimo = None
 
     def __repr__(self):
-        return "[" + str(self.primeiro) + "]"
+        return f"[{self.primeiro}]"
 
     def push(self, novo_dado):
-        """Inseri um elemento no final da fila"""
+        """Insere um elemento no final da fila"""
 
-        novo_nodo = Nodo(novo_dado)
+        novo_no = Nodo(novo_dado)
 
         if self.primeiro is None:
-            self.primeiro = novo_nodo
-            self.ultimo = novo_nodo
+            self.primeiro = novo_no
+            self.ultimo = novo_no
         else:
-            self.ultimo.proximo = novo_nodo
-            self.ultimo = novo_nodo
+            self.ultimo.proximo = novo_no
+            self.ultimo = novo_no
 
     def pop(self):
         """Remove o primeiro elemento da fila"""
 
-        assert self.primeiro is not None, "Impossível remover elemento de fila vazia"
+        assert self.primeiro is not None, "Impossível remover elemento de uma fila vazia"
 
         self.primeiro = self.primeiro.proximo
         if self.primeiro is None:
             self.ultimo = None
 
 
-def somaLista(fila: Fila) -> int:
-    if fila.primeiro is None:
+def soma_lista(corrente):
+    if not corrente:
         return 0
-    else:
-        soma = fila.primeiro.dado
-        fila.pop()
-        return soma + somaLista(fila)
+    return corrente.dado + soma_lista(corrente.proximo)
+
+
+fila = Fila()
+for i in range(15):
+    fila.push(i)
+print(f"Fila formada: {fila}")
+soma = soma_lista(fila.primeiro)
+print(f"Soma dos elementos da fila: {soma}")
+
+#Relatório#
+"""
+- O código foi refeito com o objetivo de seguir as regras da recursividade
+"""
